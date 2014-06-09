79880417a0c8a49b47b9f7263bfc3d5296e2e282
Takashi Kokubun <takashikkbn@gmail.com>
2014-06-10 01:14:35 +0900

    Create game display prototype

---
+++ tetris.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"github.com/nsf/termbox-go"
+	"strings"
+)
+
+const (
+	background = `
+    wwwwwwwwwwww wwwwww
+    w..........w w....w
+    w..........w w....w
+    w..........w w....w
+    w..........w w....w
+    w..........w wwwwww
+    w..........w
+    w..........w
+    w..........w
+    w..........w
+    w..........w
+    w..........w
+    w..........w
+    w..........w
+    w..........w
+    w..........w
+    w..........w
+    w..........w
+    w..........w
+    wwwwwwwwwwww
+	`
+)
+
+func drawByText(text string) {
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	lines := strings.Split(text, "\n")
+
+	for y, line := range lines {
+		for x, char := range line {
+			switch char {
+			case 'w':
+				termbox.SetCell(2*x-1, y, ' ', termbox.ColorDefault, termbox.ColorWhite|termbox.AttrBold)
+				termbox.SetCell(2*x, y, ' ', termbox.ColorDefault, termbox.ColorWhite|termbox.AttrBold)
+			case '.':
+				termbox.SetCell(2*x-1, y, ' ', termbox.ColorDefault, termbox.ColorBlack)
+				termbox.SetCell(2*x, y, ' ', termbox.ColorDefault, termbox.ColorBlack)
+			}
+		}
+	}
+	termbox.Flush()
+}
+
+func main() {
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	termbox.SetInputMode(termbox.InputEsc)
+	termbox.Flush()
+
+	drawByText(background)
+
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			if ev.Ch == 'q' || ev.Key == termbox.KeyCtrlC || ev.Key == termbox.KeyCtrlD {
+				return
+			}
+		}
+	}
+}
