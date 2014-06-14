f42186c9ce028e12b6cfb40c4a1d817b56115dad
Takashi Kokubun <takashikkbn@gmail.com>
2014-06-14 15:02:48 +0900

    Refactor coloring

--- view.go
+++ view.go
@@ -7,50 +7,55 @@ import (
 
 const (
 	background = `
-    wwwwwwwwwwww wwwwww
-    w..........w w....w
-    w..........w w....w
-    w..........w w....w
-    w..........w w....w
-    w..........w wwwwww
-    w..........w
-    w..........w
-    w..........w
-    w..........w
-    w..........w
-    w..........w
-    w..........w
-    w..........w
-    w..........w
-    w..........w
-    w..........w
-    w..........w
-    w..........w
-    wwwwwwwwwwww
+    WWWWWWWWWWWW WWWWWW
+    WkkkkkkkkkkW WkkkkW
+    WkkkkkkkkkkW WkkkkW
+    WkkkkkkkkkkW WkkkkW
+    WkkkkkkkkkkW WkkkkW
+    WkkkkkkkkkkW WWWWWW
+    WkkkkkkkkkkW
+    WkkkkkkkkkkW
+    WkkkkkkkkkkW
+    WkkkkkkkkkkW
+    WkkkkkkkkkkW
+    WkkkkkkkkkkW
+    WkkkkkkkkkkW
+    WkkkkkkkkkkW
+    WkkkkkkkkkkW
+    WkkkkkkkkkkW
+    WkkkkkkkkkkW
+    WkkkkkkkkkkW
+    WkkkkkkkkkkW
+    WWWWWWWWWWWW
 	`
 )
 
 func refreshScreen() {
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 
-	drawByText(background, 0, 0)
+	drawBlocks(background, 0, 0)
 
 	termbox.Flush()
 }
 
-func drawByText(text string, left, top int) {
+func drawBlocks(text string, left, top int) {
 	lines := strings.Split(text, "\n")
 
 	for y, line := range lines {
 		for x, char := range line {
-			switch char {
-			case 'w':
-				termbox.SetCell(2*left+2*x-1, top+y, ' ', termbox.ColorDefault, termbox.ColorWhite|termbox.AttrBold)
-				termbox.SetCell(2*left+2*x, top+y, ' ', termbox.ColorDefault, termbox.ColorWhite|termbox.AttrBold)
-			case '.':
-				termbox.SetCell(2*left+2*x-1, top+y, ' ', termbox.ColorDefault, termbox.ColorBlack)
-				termbox.SetCell(2*left+2*x, top+y, ' ', termbox.ColorDefault, termbox.ColorBlack)
-			}
+			termbox.SetCell(2*left+2*x-1, top+y, ' ', termbox.ColorDefault, colorByChar(char))
+			termbox.SetCell(2*left+2*x, top+y, ' ', termbox.ColorDefault, colorByChar(char))
 		}
+	}
+}
+
+func colorByChar(ch rune) termbox.Attribute {
+	switch ch {
+	case 'W':
+		return termbox.ColorWhite | termbox.AttrBold
+	case 'k':
+		return termbox.ColorBlack
+	default:
+		return termbox.ColorDefault
 	}
 }
