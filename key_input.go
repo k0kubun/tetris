abafb0460a00652699a11521f0e2c35a068e1a25
MichaelS11 <MichaelS11@users.noreply.github.com>
2017-03-21 06:36:34 -0700

    Added ProcessEvent

--- key_input.go
+++ key_input.go
@@ -6,43 +6,48 @@ import (
 
 func waitKeyInput() {
 	for {
-		switch ev := termbox.PollEvent(); ev.Type {
-		case termbox.EventKey:
-			if ev.Ch == 'q' || ev.Key == termbox.KeyCtrlC || ev.Key == termbox.KeyCtrlD {
+		event := termbox.PollEvent()
+		if event.Type == termbox.EventKey {
+			if event.Ch == 'q' || event.Key == termbox.KeyCtrlC || event.Key == termbox.KeyCtrlD {
 				return
-			} else {
-				if clock.lock {
-					continue
-				} else if clock.gameover {
-					if ev.Key == termbox.KeySpace {
-						initGame()
-						clock.start()
-					}
-					continue
-				} else if clock.paused {
-					if ev.Ch == 'p' {
-						clock.start()
-					}
-					continue
-				}
-
-				if ev.Ch == 'p' {
-					clock.pause()
-				} else if ev.Ch == 'z' {
-					currentMino.rotateLeft()
-				} else if ev.Ch == 'x' || ev.Key == termbox.KeyArrowUp {
-					currentMino.rotateRight()
-				} else if ev.Key == termbox.KeySpace {
-					currentMino.drop()
-				} else if ev.Key == termbox.KeyArrowDown {
-					currentMino.moveDown()
-				} else if ev.Key == termbox.KeyArrowLeft {
-					currentMino.moveLeft()
-				} else if ev.Key == termbox.KeyArrowRight {
-					currentMino.moveRight()
-				}
 			}
+			ProcessEvent(&event)
 		}
-		refreshScreen()
 	}
+}
+
+func ProcessEvent(event *termbox.Event) {
+
+	if clock.lock {
+		return
+	} else if clock.gameover {
+		if event.Key == termbox.KeySpace {
+			initGame()
+			clock.start()
+		}
+		return
+	} else if clock.paused {
+		if event.Ch == 'p' {
+			clock.start()
+		}
+		return
+	}
+
+	if event.Ch == 'p' {
+		clock.pause()
+	} else if event.Ch == 'z' {
+		currentMino.rotateLeft()
+	} else if event.Ch == 'x' || event.Key == termbox.KeyArrowUp {
+		currentMino.rotateRight()
+	} else if event.Key == termbox.KeySpace {
+		currentMino.drop()
+	} else if event.Key == termbox.KeyArrowDown {
+		currentMino.moveDown()
+	} else if event.Key == termbox.KeyArrowLeft {
+		currentMino.moveLeft()
+	} else if event.Key == termbox.KeyArrowRight {
+		currentMino.moveRight()
+	}
+
+	refreshScreen()
 }
